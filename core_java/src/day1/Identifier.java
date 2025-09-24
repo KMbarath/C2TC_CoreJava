7669d6ed3789cba1234bc83459c7330dc9e39a1f
BARATH KM <BARATH KM@BARATH-PC>
2025-09-24 06:45:26 +0530

    Task Completed

--- core_java/src/day1/Identifier.java
+++ core_java/src/day1/Identifier.java
@@ -3,8 +3,8 @@ package day1;
 public class Identifier {
 	public static void main(String[] args) {
 		
-		int $number = 20; 
-		System.out.println("value of the number variable is : "+ $number);
+		int number = 20; 
+		System.out.println("value of the number variable is : "+ number);
 	
 		String studentName="Aniket";
 		System.out.println("value is : "+ studentName);
