07736765a31c0a199bbf4ceb3eb29b415820cbed
BARATH KM <BARATH KM@BARATH-PC>
2025-09-27 17:30:28 +0530

    Day3 Completed

---
+++ core_java/src/day3/encapsulation/Encapsulation.java
@@ -0,0 +1,15 @@
+package day3.encapsulation;
+
+public class Encapsulation {
+public static void main(String[] args) {
+		
+		//object creation 
+		OopsConcept obj = new OopsConcept();
+		obj.setSerialNum(4);
+		obj.setName("BARATH");
+		obj.setAge(20);
+		System.out.println(obj);
+		
+	}
+
+}
