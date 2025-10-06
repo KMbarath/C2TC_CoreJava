cc216a8ceb4c712e8f34fcc4e2aa0acdc59941f3
BARATH KM <BARATH KM@BARATH-PC>
2025-10-06 19:07:33 +0530

    Day9 Completed

--- core_java/src/com/tnsif/ifet/day9/WrapperClass.java
+++ core_java/src/com/tnsif/ifet/day9/WrapperClass.java
@@ -1,21 +1,22 @@
 package com.tnsif.ifet.day9;
 
 public class WrapperClass {
-	public static void main(String[] args) {
-		// Unboxing
-		Integer i = new Integer(10);
-		System.out.println(i);
-		int b = i.intValue();
-		System.out.println(b);
+    public static void main(String[] args) {
+        // Boxing (recommended)
+        Integer i = Integer.valueOf(10);
+        System.out.println("Boxed Integer: " + i);
 
-		// Without using intValue()
-		int c = i;
-		System.out.println(c);
-		
-		//Autoboxing
-		int a = 100;
-		Integer i1 = a;
-		System.out.println(i1);
-	}
+        // Unboxing
+        int b = i.intValue();
+        System.out.println("Unboxed using intValue(): " + b);
 
+        // Auto-unboxing
+        int c = i;
+        System.out.println("Auto-unboxed: " + c);
+
+        // Autoboxing
+        int a = 100;
+        Integer i1 = a;
+        System.out.println("Autoboxed Integer: " + i1);
+    }
 }
