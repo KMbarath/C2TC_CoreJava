62934ce8199c05ed9c45f3588b645fd5b3a4d2db
BARATH KM <BARATH KM@BARATH-PC>
2025-10-24 21:41:33 +0530

    Day 10,11,12,13 Completed

---
+++ core_java/src/com/tnsif/ifet/day10/Driver.java
@@ -0,0 +1,35 @@
+package com.tnsif.ifet.day10;
+
+public class Driver {
+	public static void main(String args[]) {
+		int n = 10;
+		int a[]; // declaration
+		a = new int[n]; // instantiation
+
+		ArrayOperations.printArray(a);
+
+		for (int i = 0; i < a.length; i++)
+			a[i] = 5 * i;
+
+		ArrayOperations.printArray(a);
+
+		int b[] = { 10, 20, 30, 40, 50 }; // initialization at the time of declaration
+		ArrayOperations.printArray(b);
+
+		// calling variable argument function
+		System.out.println("sum of array elements is : " + ArrayOperations.getSum(b));
+		System.out.println("sum of array elements is : " + ArrayOperations.getSum(10, 20));
+
+		b[2] = 999;
+		ArrayOperations.printArray(b);
+
+		System.out.println("Odd numbers : " + ArrayOperations.getOddCount(b) + "\tEven numbers : "
+				+ ArrayOperations.getEvenCount(b));
+		System.out.println();
+		System.out.println("---------------------------------------");
+
+		int c[];
+
+	}
+
+}
